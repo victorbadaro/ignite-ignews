a716c7394450e4af1881724b7846dfa2a56945d8
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-04-03 18:09:06 -0300

    chore: change Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IcDlACFieFUZXbKLMnmjd7B');
 
   const product = {
@@ -50,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   };
 
   return {
-    props: { product }
+    props: { product },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 }
