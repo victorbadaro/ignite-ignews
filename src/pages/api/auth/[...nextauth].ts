37fdb8d58f478542d1a9b50741cd23d982ad4f4a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-04-08 13:00:49 -0300

    chore: change next auth api route

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -11,8 +13,17 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn(user, account, profile) {
-      console.log(user);
-      return true;
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(
+          q.Collection('users'), { data: {email} })
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
     }
   }
 });
