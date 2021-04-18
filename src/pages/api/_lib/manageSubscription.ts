b31606c7f965657b8e0f01125f50fed2e59b04ab
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-04-17 23:59:25 -0300

    chore: change manageSubscription

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,3 +1,4 @@
 export function saveSubscription(subscriptionId: string, customerId: string) {
     // Buscar o usuário no banco do FaunaDB com o ID {customerId}
+    // Salvar os dados da subscription no FaunaDB
 }
