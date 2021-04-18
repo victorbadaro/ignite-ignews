9eaacfe377c72d62f9f0bc3400d9ba9d4f0bde48
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-04-18 05:35:33 -0300

    chore: add webhook listeners

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,4 +1,50 @@
-export function saveSubscription(subscriptionId: string, customerId: string) {
-    // Buscar o usuário no banco do FaunaDB com o ID {customerId}
-    // Salvar os dados da subscription no FaunaDB
+import { query as q } from 'faunadb';
+import { fauna } from "../../../services/fauna";
+import { stripe } from '../../../services/stripe';
+
+export async function saveSubscription(subscriptionId: string, customerId: string, createAction = false) {
+    const userRef = await fauna.query(
+        q.Select(
+            'ref',
+            q.Get(
+                q.Match(
+                    q.Index('user_by_stripe_customer_id'),
+                    customerId
+                )
+            )
+        )
+    );
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+    const subscriptionData = {
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        priceId: subscription.items.data[0].price.id,
+
+    }
+
+    if(createAction) {
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                { data: subscriptionData }
+            )
+        );
+    } else {
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    'ref',
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscriptionId
+                        )
+                    )
+                ),
+                { data: subscriptionData }
+            )
+        );
+    }
 }
