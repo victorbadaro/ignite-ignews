fa683924e6569a7c7587eb26ab7773dd83a22a21
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-05-10 05:24:37 -0300

    chore: change post page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -39,6 +39,16 @@ export default function Post({ post }: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
     const session = await getSession({ req });
     const { slug } = params;
+
+    if(!session?.activeSubscription) {
+        return {
+            redirect: {
+                destination: `/posts/preview/${slug}`,
+                permanent: false
+            }
+        }
+    }
+
     const prismic = getPrismicClient(req);
     const response = await prismic.getByUID('post', String(slug), {});
     const post = {
